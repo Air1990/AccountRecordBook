8193e35192cb841f1fc0d6dbc0cbf8f22acb1d67
Air1990 <759394163@qq.com>
2015-05-17 18:27:32 +0800

    second commit

---
+++ src/main/java/com/wang/account/record/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wang.account.record;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.asus.myfirstapplication.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
